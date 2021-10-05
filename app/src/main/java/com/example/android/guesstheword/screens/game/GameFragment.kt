b6354b31f280f46bf3fc01e07cbda1da3148d6b2
ElenaSharpilova <esharpilova@gmail.com>
2021-10-05 14:58:49 +0600

    add ViewModel and LiveData

---
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -0,0 +1,59 @@
+package com.example.android.guesstheword.screens.game
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import com.example.android.guesstheword.R
+import com.example.android.guesstheword.databinding.GameFragmentBinding
+
+class GameFragment : Fragment() {
+
+    private lateinit var viewModel: GameViewModel
+    private lateinit var binding: GameFragmentBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.game_fragment,
+            container,
+            false
+        )
+
+        // Get the viewmodel
+        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
+
+        binding.correctButton.setOnClickListener {
+            viewModel.onCorrect()
+            updateWordText()
+        }
+        binding.skipButton.setOnClickListener {
+            viewModel.onSkip()
+            updateWordText()
+        }
+
+        viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
+            binding.scoreText.text = newScore.toString()
+        })
+
+        updateWordText()
+        return binding.root
+    }
+
+    fun gameFinished() {
+        val action = GameFragmentDirections.actionGameToScore(viewModel.score.value ?: 0)
+        findNavController(this).navigate(action)
+    }
+
+    private fun updateWordText() {
+        binding.wordText.text = viewModel.word
+    }
+
+}
+
---
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -0,0 +1,68 @@
+package com.example.android.guesstheword.screens.game
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+
+    // The current word
+    var word = ""
+
+    // The current score
+    var score = MutableLiveData<Int>()
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
+    init {
+        resetList()
+        nextWord()
+        score.value = 0
+    }
+
+    private fun resetList() {
+        wordList = mutableListOf(
+            "queen",
+            "hospital",
+            "basketball",
+            "cat",
+            "change",
+            "snail",
+            "soup",
+            "calendar",
+            "sad",
+            "desk",
+            "guitar",
+            "home",
+            "railway",
+            "zebra",
+            "jelly",
+            "car",
+            "crow",
+            "trade",
+            "bag",
+            "roll",
+            "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    private fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+            // gameFinished() should happen here
+        } else {
+            word = wordList.removeAt(0)
+        }
+    }
+
+    fun onSkip() {
+        score.value = (score.value)?.minus(1)
+        nextWord()
+    }
+
+    fun onCorrect() {
+        score.value = (score.value)?.plus(1)
+        nextWord()
+    }
+}
